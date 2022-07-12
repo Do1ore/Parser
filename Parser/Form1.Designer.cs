b1e0431c7b4df6442f10881a51bdca89dbe020fe
Владислав Харламов <vladxreal@gmail.com>
2022-07-12 17:28:43 +0300

    Добавлен курс на продажу Функции немного изменны Встечаются вылеты с сайтом Alfabank

--- Parser/Form1.Designer.cs
+++ Parser/Form1.Designer.cs
@@ -28,20 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.ABank = new System.Windows.Forms.Label();
             this.DabrBank = new System.Windows.Forms.Label();
             this.SolutionBank = new System.Windows.Forms.Label();
             this.BelBank = new System.Windows.Forms.Label();
             this.BelagrBank = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.BelagrBankOut = new System.Windows.Forms.Label();
+            this.BelBankOut = new System.Windows.Forms.Label();
+            this.SolutionBankOut = new System.Windows.Forms.Label();
+            this.DabrBankOut = new System.Windows.Forms.Label();
+            this.ABankOut = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -50,7 +54,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label1.Font = new System.Drawing.Font("Bahnschrift", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(44, 44);
+            this.label1.Location = new System.Drawing.Point(44, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(434, 77);
             this.label1.TabIndex = 0;
@@ -61,8 +65,8 @@
             this.ABank.AutoSize = true;
             this.ABank.BackColor = System.Drawing.Color.Transparent;
             this.ABank.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ABank.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ABank.Location = new System.Drawing.Point(559, 215);
+            this.ABank.ForeColor = System.Drawing.Color.Black;
+            this.ABank.Location = new System.Drawing.Point(282, 192);
             this.ABank.Name = "ABank";
             this.ABank.Size = new System.Drawing.Size(173, 33);
             this.ABank.TabIndex = 1;
@@ -73,8 +77,8 @@
             this.DabrBank.AutoSize = true;
             this.DabrBank.BackColor = System.Drawing.Color.Transparent;
             this.DabrBank.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.DabrBank.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.DabrBank.Location = new System.Drawing.Point(559, 262);
+            this.DabrBank.ForeColor = System.Drawing.Color.Black;
+            this.DabrBank.Location = new System.Drawing.Point(282, 239);
             this.DabrBank.Name = "DabrBank";
             this.DabrBank.Size = new System.Drawing.Size(173, 33);
             this.DabrBank.TabIndex = 2;
@@ -85,8 +89,8 @@
             this.SolutionBank.AutoSize = true;
             this.SolutionBank.BackColor = System.Drawing.Color.Transparent;
             this.SolutionBank.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SolutionBank.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.SolutionBank.Location = new System.Drawing.Point(559, 305);
+            this.SolutionBank.ForeColor = System.Drawing.Color.Black;
+            this.SolutionBank.Location = new System.Drawing.Point(282, 282);
             this.SolutionBank.Name = "SolutionBank";
             this.SolutionBank.Size = new System.Drawing.Size(173, 33);
             this.SolutionBank.TabIndex = 3;
@@ -97,8 +101,8 @@
             this.BelBank.AutoSize = true;
             this.BelBank.BackColor = System.Drawing.Color.Transparent;
             this.BelBank.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.BelBank.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.BelBank.Location = new System.Drawing.Point(559, 353);
+            this.BelBank.ForeColor = System.Drawing.Color.Black;
+            this.BelBank.Location = new System.Drawing.Point(282, 330);
             this.BelBank.Name = "BelBank";
             this.BelBank.Size = new System.Drawing.Size(173, 33);
             this.BelBank.TabIndex = 4;
@@ -110,30 +114,19 @@
             this.BelagrBank.BackColor = System.Drawing.Color.Transparent;
             this.BelagrBank.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.BelagrBank.ForeColor = System.Drawing.Color.Black;
-            this.BelagrBank.Location = new System.Drawing.Point(559, 400);
+            this.BelagrBank.Location = new System.Drawing.Point(282, 377);
             this.BelagrBank.Name = "BelagrBank";
             this.BelagrBank.Size = new System.Drawing.Size(173, 33);
             this.BelagrBank.TabIndex = 5;
             this.BelagrBank.Text = "Беларусбанк";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Parser.Properties.Resources.Artboard_1;
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(-7, -4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(792, 495);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(305, 400);
+            this.label7.ForeColor = System.Drawing.Color.Maroon;
+            this.label7.Location = new System.Drawing.Point(28, 377);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(238, 33);
             this.label7.TabIndex = 11;
@@ -144,8 +137,8 @@
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label8.Location = new System.Drawing.Point(305, 353);
+            this.label8.ForeColor = System.Drawing.Color.Maroon;
+            this.label8.Location = new System.Drawing.Point(28, 330);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(173, 33);
             this.label8.TabIndex = 10;
@@ -156,8 +149,8 @@
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label9.Location = new System.Drawing.Point(305, 305);
+            this.label9.ForeColor = System.Drawing.Color.Maroon;
+            this.label9.Location = new System.Drawing.Point(28, 282);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(193, 33);
             this.label9.TabIndex = 9;
@@ -168,8 +161,8 @@
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label10.Location = new System.Drawing.Point(305, 262);
+            this.label10.ForeColor = System.Drawing.Color.Maroon;
+            this.label10.Location = new System.Drawing.Point(28, 239);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(199, 33);
             this.label10.TabIndex = 8;
@@ -180,18 +173,105 @@
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.Color.Transparent;
             this.label11.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label11.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label11.Location = new System.Drawing.Point(305, 215);
+            this.label11.ForeColor = System.Drawing.Color.Maroon;
+            this.label11.Location = new System.Drawing.Point(28, 192);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(153, 33);
             this.label11.TabIndex = 7;
             this.label11.Text = "Альфабанк";
+            // 
+            // BelagrBankOut
+            // 
+            this.BelagrBankOut.AutoSize = true;
+            this.BelagrBankOut.BackColor = System.Drawing.Color.Transparent;
+            this.BelagrBankOut.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BelagrBankOut.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.BelagrBankOut.Location = new System.Drawing.Point(534, 377);
+            this.BelagrBankOut.Name = "BelagrBankOut";
+            this.BelagrBankOut.Size = new System.Drawing.Size(238, 33);
+            this.BelagrBankOut.TabIndex = 16;
+            this.BelagrBankOut.Text = "Белагропромбанк";
+            // 
+            // BelBankOut
+            // 
+            this.BelBankOut.AutoSize = true;
+            this.BelBankOut.BackColor = System.Drawing.Color.Transparent;
+            this.BelBankOut.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BelBankOut.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.BelBankOut.Location = new System.Drawing.Point(534, 330);
+            this.BelBankOut.Name = "BelBankOut";
+            this.BelBankOut.Size = new System.Drawing.Size(173, 33);
+            this.BelBankOut.TabIndex = 15;
+            this.BelBankOut.Text = "Беларусбанк";
+            // 
+            // SolutionBankOut
+            // 
+            this.SolutionBankOut.AutoSize = true;
+            this.SolutionBankOut.BackColor = System.Drawing.Color.Transparent;
+            this.SolutionBankOut.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SolutionBankOut.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.SolutionBankOut.Location = new System.Drawing.Point(534, 282);
+            this.SolutionBankOut.Name = "SolutionBankOut";
+            this.SolutionBankOut.Size = new System.Drawing.Size(193, 33);
+            this.SolutionBankOut.TabIndex = 14;
+            this.SolutionBankOut.Text = "Банк Решение";
+            // 
+            // DabrBankOut
+            // 
+            this.DabrBankOut.AutoSize = true;
+            this.DabrBankOut.BackColor = System.Drawing.Color.Transparent;
+            this.DabrBankOut.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DabrBankOut.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.DabrBankOut.Location = new System.Drawing.Point(534, 239);
+            this.DabrBankOut.Name = "DabrBankOut";
+            this.DabrBankOut.Size = new System.Drawing.Size(173, 33);
+            this.DabrBankOut.TabIndex = 13;
+            this.DabrBankOut.Text = "Беларусбанк";
+            // 
+            // ABankOut
+            // 
+            this.ABankOut.AutoSize = true;
+            this.ABankOut.BackColor = System.Drawing.Color.Transparent;
+            this.ABankOut.Font = new System.Drawing.Font("Bahnschrift", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ABankOut.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.ABankOut.Location = new System.Drawing.Point(534, 192);
+            this.ABankOut.Name = "ABankOut";
+            this.ABankOut.Size = new System.Drawing.Size(173, 33);
+            this.ABankOut.TabIndex = 12;
+            this.ABankOut.Text = "Беларусбанк";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(295, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 19);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Покупка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(551, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 19);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Продажа";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 484);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BelagrBankOut);
+            this.Controls.Add(this.BelBankOut);
+            this.Controls.Add(this.SolutionBankOut);
+            this.Controls.Add(this.DabrBankOut);
+            this.Controls.Add(this.ABankOut);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label9);
@@ -203,11 +283,9 @@
             this.Controls.Add(this.DabrBank);
             this.Controls.Add(this.ABank);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,12 +299,18 @@
         private System.Windows.Forms.Label SolutionBank;
         private System.Windows.Forms.Label BelBank;
         private System.Windows.Forms.Label BelagrBank;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label BelagrBankOut;
+        private System.Windows.Forms.Label BelBankOut;
+        private System.Windows.Forms.Label SolutionBankOut;
+        private System.Windows.Forms.Label DabrBankOut;
+        private System.Windows.Forms.Label ABankOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Parser/Form1.cs
+++ Parser/Form1.cs
@@ -18,10 +18,19 @@ namespace Parser
     public partial class Form1 : Form
     {
         public string _Alfabank { get; set; }
+        public string _AlfabankOut { get; set; }
+
         public string _Belarus { get; set; }
+        public string _BelarusOut { get; set; }
+
         public string _ReshenieBank { get; set; }
+        public string _ReshenieBankOut { get; set; }
+
         public string _DabrabBank { get; set; }
+        public string _DabrabankOut { get; set; }
+
         public string _BelapbBank { get; set; }
+        public string _BelapbBankOut { get; set; }
 
         static int a = 0;
         
@@ -45,10 +54,13 @@ namespace Parser
                 dynamic data = JObject.Parse(jsonArray[0].ToString());
                 _Belarus = Convert.ToString(data.USD_in);
 
+                _BelarusOut = Convert.ToString(data.USD_out);
+
                 
 
             });
             BelBank.Text = _Belarus;
+            BelBankOut.Text = _BelarusOut;
 
 
         }
@@ -72,17 +84,21 @@ namespace Parser
 
         async public void AlfaBank()
         {
-            
-            await Task.Run(() =>
-            {
-                IWebDriver webDriver = new ChromeDriver();
-                webDriver.Url = "https://www.alfabank.by/";
+            IWebDriver webDriver = new ChromeDriver();
+            await Task.Run(() => {
+                
+                webDriver.Url = ("https://www.alfabank.by");
                 var parsed = webDriver.FindElement(By.CssSelector(@"#app > div > div:nth-child(4) > section.section.no-margin > div > div.currency > div.currency__wrapper > div > div.v-data-table.currency-table.text-right.theme--light > div > table > tbody > tr:nth-child(1) > td:nth-child(3) > span:nth-child(2)")).GetAttribute("textContent");
-                webDriver.Quit();
                 _Alfabank = parsed.ToString();
 
+                Task.Delay(500);
+                parsed = webDriver.FindElement(By.CssSelector(@"#app > div > div:nth-child(4) > section.section.no-margin > div > div.currency > div.currency__wrapper > div > div.v-data-table.currency-table.text-right.theme--light > div > table > tbody > tr:nth-child(1) > td:nth-child(4) > span:nth-child(2)")).GetAttribute("textContent");
+                _AlfabankOut = parsed.ToString();
+                webDriver.Quit();
+                return Task.CompletedTask;
             });
             ABank.Text = _Alfabank;
+            ABankOut.Text = _AlfabankOut;
         }
 
         async public void DabrabytBank()
@@ -93,24 +109,29 @@ namespace Parser
                 System.Net.WebClient wc = new System.Net.WebClient();
                 string Responce = wc.DownloadString("https://bankdabrabyt.by");
                 _DabrabBank = System.Text.RegularExpressions.Regex.Match(Responce, @"</td><td class=""rate-table__sale"">([0-9]+\.[0-9]+)").Groups[1].Value;
+                _DabrabankOut = System.Text.RegularExpressions.Regex.Match(Responce, @"</td><td class=""rate-table__purchase"">([0-9]+\.[0-9]+)").Groups[1].Value;
             });
 
             DabrBank.Text = _DabrabBank;
+            DabrBankOut.Text = _DabrabankOut;
         }
 
         async public void BankReshenie()
         {
-            string Rate = "";
+            
             await Task.Run(() =>
             {
                 IWebDriver webDriver = new ChromeDriver();
                 webDriver.Url = "https://rbank.by/";
                 var parsed = webDriver.FindElement(By.CssSelector(@"body > div.wrapper > main > div:nth-child(3) > div > div > div > div.currency-block > div.currency-block__body > div.currency-block__table > div > div > div.currency-table__body-list.js-list-1.active > div:nth-child(3) > span.down")).GetAttribute("textContent");
+                _ReshenieBank = parsed.ToString();
+                parsed = webDriver.FindElement(By.CssSelector(@"body > div.wrapper > main > div:nth-child(3) > div > div > div > div.currency-block > div.currency-block__body > div.currency-block__table > div > div > div.currency-table__body-list.js-list-1.active > div:nth-child(3) > span:nth-child(2)")).GetAttribute("textContent");
+                _ReshenieBankOut = parsed.ToString();
                 webDriver.Quit();
-                Rate = parsed.ToString();
 
             });
-            SolutionBank.Text = Rate;
+            SolutionBank.Text = _ReshenieBank;
+            SolutionBankOut.Text = _ReshenieBankOut;
         }
         
 
@@ -119,29 +140,36 @@ namespace Parser
             IWebDriver webDriver = new ChromeDriver();
             webDriver.Url = "https://www.belapb.by";
 
-            await Task.Run(() =>
+            await Task.Run(async () =>
             {
                 for (int i = 0; i < 10; i++)
                 {
                     try
                     {
-                        webDriver.FindElement(By.XPath(@"//*[@id=""aj_tab2""]/table/tbody/tr[2]/td[2]/a")).GetCssValue("a");
+                        webDriver.FindElement(By.XPath(@"//*[@id=""aj_tab2""]/table/tbody/tr[2]/td[2]/a")).GetCssValue("textContent");
                     }
                     catch (Exception)
                     {
-                        Task.Delay(1000);
+                        await Task.Delay(1000);
                         break;
 
                     }
                 }
-
-                var parsed = webDriver.FindElement(By.CssSelector(@"#aj_tab2 > table > tbody > tr:nth-child(2) > td:nth-child(2) > a")).GetAttribute("textContent");
-                _BelapbBank = parsed.ToString();
-                webDriver.Quit();
             });
 
-            BelagrBank.Text = _BelapbBank; 
-           
+
+            var parsed = webDriver.FindElement(By.CssSelector(@"#aj_tab2 > table > tbody > tr:nth-child(2) > td:nth-child(2) > a")).GetAttribute("textContent");
+                _BelapbBank = parsed.ToString();
+
+                
+            parsed = webDriver.FindElement(By.CssSelector(@"#aj_tab2 > table > tbody > tr:nth-child(2) > td:nth-child(3) > a")).GetAttribute("textContent");
+            _BelapbBankOut = parsed.ToString();
+            webDriver.Quit();
+                    
+               
+            
+            BelagrBank.Text = _BelapbBank;
+            BelagrBankOut.Text = _BelapbBankOut;
             
         }
         
